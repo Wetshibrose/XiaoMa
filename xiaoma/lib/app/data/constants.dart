aaad30488bc114b6be08db273b300235e7dc2fc8
Jaydrey <jaribwetshi64@gmail.com>
2023-07-16 23:25:22 +0300

    feat(constants): create constant file and add static variables

---
+++ xiaoma/lib/app/data/constants.dart
@@ -0,0 +1,9 @@
+class AppConstants {
+//  shared preferences meta data
+  static const String userOnboarded = "userOnboarded";
+  static const String userData = "userData";
+
+//
+}
+
+enum MapSearchFieldStatus { source, destination, saved }
