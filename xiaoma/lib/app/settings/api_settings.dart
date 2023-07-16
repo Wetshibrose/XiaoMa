1d16483d129fadc3f23be573b665a869069cf035
Jaydrey <jaribwetshi64@gmail.com>
2023-07-16 23:27:22 +0300

    feat(settings): create the static variables for google maps url

---
+++ xiaoma/lib/app/settings/api_settings.dart
@@ -0,0 +1,8 @@
+class ApiSettings {
+//  google maps
+  static const scheme = "https";
+  static const autocompletionBase = "https://maps.googleapis.com";
+  static const autocompletionPath = "maps/api/place/autocomplete/json";
+
+  static const contentType = "application/json";
+}
