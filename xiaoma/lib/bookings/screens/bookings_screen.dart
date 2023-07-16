650b1d9bdc9db9a8011d1ade2b3d6db7c13c3468
Jaydrey <jaribwetshi64@gmail.com>
2023-07-16 23:40:33 +0300

    feat(screen): initialize the bookings screen

---
+++ xiaoma/lib/bookings/screens/bookings_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class BookingScreen extends StatefulWidget {
+  const BookingScreen({super.key});
+
+  @override
+  State<BookingScreen> createState() => _BookingScreenState();
+}
+
+class _BookingScreenState extends State<BookingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: const Center(
+          child: Text("Booking"),
+        ),
+      ),
+    );
+  }
+}
