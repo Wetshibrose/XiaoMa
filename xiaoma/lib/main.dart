077d38eb95e4ad853a146ba99c2209206cb12c36
Jaydrey <jaribwetshi64@gmail.com>
2023-07-30 00:34:10 +0300

    feat(main): get status for onboarded and authentication
    get status for onboarded and authentication to determine routing screens

--- xiaoma/lib/main.dart
+++ xiaoma/lib/main.dart
@@ -1,15 +1,18 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+// ignore_for_file: public_member_api_docs, sort_constructors_first, no_leading_underscores_for_local_identifiers
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+// app pack
+import 'package:xiaoma/app/app.dart';
+import 'package:xiaoma/auth/auth.dart';
+import 'package:xiaoma/dashboard/screens/dashboard_screen.dart';
 import 'package:xiaoma/onboarding/onboarding.dart';
-import 'package:xiaoma/profile/profile.dart';
 // utils
 import 'package:xiaoma/utils/utils.dart';
 
 // theme
-import 'dashboard/screens/screens.dart';
-import 'themes/themes.dart';
+import 'app/themes/themes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,30 +29,52 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  // repos
+  final _appStore = locator<AppStores>();
+
+  // functions
+  String initialRoute() {
+    if (_appStore.isOnBoarded && _appStore.isAuthenticated) {
+      return DashboardScreen.routeName;
+    }
+    if (!_appStore.isOnBoarded) {
+      return OnBoardScreen.routeName;
+    }
+    if (_appStore.isOnBoarded && !_appStore.isAuthenticated) {
+      return LoginScreen.routeName;
+    }
+    return OnBoardScreen.routeName;
+  }
+
   @override
   void initState() {
+    _appStore.initializeApp();
     super.initState();
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        final focusScope = FocusScope.of(context);
-        if (!focusScope.hasPrimaryFocus) {
-          focusScope.unfocus();
-          FocusManager.instance.primaryFocus?.unfocus();
-        }
+    return Observer(
+      builder: (_) {
+        final _routeName = initialRoute();
+        return GestureDetector(
+          onTap: () {
+            final focusScope = FocusScope.of(context);
+            if (!focusScope.hasPrimaryFocus) {
+              focusScope.unfocus();
+              FocusManager.instance.primaryFocus?.unfocus();
+            }
+          },
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'XiaoMa',
+            theme: appTheme,
+            initialRoute: _routeName,
+            onGenerateRoute: handleRoutes,
+          ),
+        );
       },
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: xiamaTheme,
-        home: const DashboardScreen(),
-        // home: const PromoCodeScreen(),
-        onGenerateRoute: handleRoutes,
-      ),
     );
   }
 }
