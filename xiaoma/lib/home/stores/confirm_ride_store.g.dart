fa983b9bb85c765c762a23e8d7edec18ea11bd04
Jaydrey <jaribwetshi64@gmail.com>
2023-07-31 11:43:25 +0300

    feat(store): add the store for the confirm ride screen

---
+++ xiaoma/lib/home/stores/confirm_ride_store.g.dart
@@ -0,0 +1,32 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'confirm_ride_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$ConfirmRideStore on _ConfirmRideStore, Store {
+  late final _$_ConfirmRideStoreActionController =
+      ActionController(name: '_ConfirmRideStore', context: context);
+
+  @override
+  void changeStartTime({required DateTime value}) {
+    final _$actionInfo = _$_ConfirmRideStoreActionController.startAction(
+        name: '_ConfirmRideStore.changeStartTime');
+    try {
+      return super.changeStartTime(value: value);
+    } finally {
+      _$_ConfirmRideStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+
+    ''';
+  }
+}
