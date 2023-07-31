81833a493cdd55e0167b3cd96328c8bb537a0a50
Jaydrey <jaribwetshi64@gmail.com>
2023-07-31 11:38:51 +0300

    feat(dashboard): create xiaoma dashboard with bottom nav
    create xiaoma dashboard with bottom navigation bar

--- xiaoma/lib/dashboard/screens/dashboard_screen.dart
+++ xiaoma/lib/dashboard/screens/dashboard_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:xiaoma/account/account.dart';
 import 'package:xiaoma/bookings/screens/bookings_screen.dart';
 import 'package:xiaoma/chat/screens/chat_screen.dart';
-import 'package:xiaoma/constants/constants.dart';
+import 'package:xiaoma/app/constants/constants.dart';
 import 'package:xiaoma/home/screens/home_screen.dart';
-import 'package:xiaoma/profile/screens/profile_screen.dart';
-import 'package:xiaoma/themes/themes.dart';
+import 'package:xiaoma/app/themes/themes.dart';
 import 'package:xiaoma/wallet/screens/wallet_screen.dart';
 
 class DashboardScreen extends StatefulWidget {
@@ -23,14 +23,13 @@ class _DashboardScreenState extends State<DashboardScreen> {
 
   // states
   int _currentIndex = 0;
-  PanelState _panelState = PanelState.CLOSED;
   // screens
   final _screens = [
     const HomeScreen(),
     const BookingScreen(),
     const ChatScreen(),
     const WalletScreen(),
-    const ProfileScreen(),
+    const AccountScreen(),
   ];
 
   @override
@@ -57,89 +56,89 @@ class _DashboardScreenState extends State<DashboardScreen> {
             _currentIndex = newIndex;
           });
         },
-        backgroundColor: XiamaColors.white,
+        backgroundColor: AppColors.white,
         elevation: 0,
         type: BottomNavigationBarType.fixed,
         selectedLabelStyle: const TextStyle(
-          color: XiamaColors.green1,
+          color: AppColors.green1,
           fontSize: 16,
         ),
         unselectedLabelStyle: const TextStyle(
-          color: XiamaColors.grey2,
+          color: AppColors.grey2,
           fontSize: 14,
         ),
         showSelectedLabels: true,
         showUnselectedLabels: true,
         items: [
           BottomNavigationBarItem(
-            label: XiamaConst.BOTTOM_NAVIGATION_1,
+            label: AppConstants.BOTTOM_NAVIGATION_1,
             icon: SvgPicture.asset(
-              XiamaConst.NAVS_1,
+              AppConstants.NAVS_1,
               height: 30,
               width: 15,
-              color: XiamaColors.grey2,
+              color: AppColors.grey2,
             ),
             activeIcon: SvgPicture.asset(
-              XiamaConst.NAVS_1,
+              AppConstants.NAVS_1,
               height: 30,
               width: 15,
-              color: XiamaColors.green1,
+              color: AppColors.green1,
             ),
           ),
           BottomNavigationBarItem(
-            label: XiamaConst.BOTTOM_NAVIGATION_2,
+            label: AppConstants.BOTTOM_NAVIGATION_2,
             icon: SvgPicture.asset(
-              XiamaConst.NAVS_2,
+              AppConstants.NAVS_2,
               height: 35,
               width: 20,
             ),
             activeIcon: SvgPicture.asset(
-              XiamaConst.NAVS_2,
+              AppConstants.NAVS_2,
               height: 30,
               width: 15,
-              color: XiamaColors.green1,
+              color: AppColors.green1,
             ),
           ),
           BottomNavigationBarItem(
-            label: XiamaConst.BOTTOM_NAVIGATION_3,
+            label: AppConstants.BOTTOM_NAVIGATION_3,
             icon: SvgPicture.asset(
-              XiamaConst.NAVS_3,
+              AppConstants.NAVS_3,
               height: 35,
               width: 20,
             ),
             activeIcon: SvgPicture.asset(
-              XiamaConst.NAVS_3,
+              AppConstants.NAVS_3,
               height: 30,
               width: 15,
-              color: XiamaColors.green1,
+              color: AppColors.green1,
             ),
           ),
           BottomNavigationBarItem(
-            label: XiamaConst.BOTTOM_NAVIGATION_4,
+            label: AppConstants.BOTTOM_NAVIGATION_4,
             icon: SvgPicture.asset(
-              XiamaConst.NAVS_4,
+              AppConstants.NAVS_4,
               height: 35,
               width: 20,
             ),
             activeIcon: SvgPicture.asset(
-              XiamaConst.NAVS_4,
+              AppConstants.NAVS_4,
               height: 30,
               width: 15,
-              color: XiamaColors.green1,
+              color: AppColors.green1,
             ),
           ),
           BottomNavigationBarItem(
-            label: XiamaConst.BOTTOM_NAVIGATION_5,
+            label: AppConstants.BOTTOM_NAVIGATION_5,
             icon: SvgPicture.asset(
-              XiamaConst.NAVS_5,
+              AppConstants.NAVS_5,
               height: 35,
               width: 20,
             ),
             activeIcon: SvgPicture.asset(
-              XiamaConst.NAVS_5,
+              AppConstants.NAVS_5,
               height: 30,
               width: 15,
-              color: XiamaColors.green1,
+              color: AppColors.green1,
             ),
           ),
         ],
